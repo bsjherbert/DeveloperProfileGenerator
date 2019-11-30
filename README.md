2fed4ca2dbee8e9b4c76df9e852161bedcd00de0
Steven <bsjherbert@gmail.com>
2019-11-30 16:21:05 -0500

    Finished DPG

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # DeveloperProfileGenerator
+Node.JS HomeWork Assignment (Developer Profile Generator)
+
+# Running Tests
+1. Open up the terminal
+2. Navigate into the correct folder
+3. Type (node index.js) and press enter
+4. Answer prompt number one with your github username
+5. Answer prompt number two with your favorite color
+
+# Built With
+JavaScript
+Node.js
+Inquirer (Npm)
+
+# Authors
+Brad Herbert - main author
+
+# Released under MIT License
+
+Copyright (c)2019
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+# Acknowledgements
+Inspiration from UNCC Bootcamp
+
+# Link
+
+https://bsjherbert.github.io/DeveloperProfileGenerator/
---
+++ index.js
@@ -0,0 +1,57 @@
+const inquirer = require("inquirer");
+const axios = require("axios");
+
+
+ 
+
+var fs = require('fs')
+var createHTML = require('create-html')
+ 
+
+
+const client_id = "51a029b972e0b63da4fb";
+const client_secret = "92ded2dee8393d85d3ccad77425cafc03006ef84";
+
+
+inquirer.prompt([
+    {
+        type: "input",
+        message: "What is your github user name?",
+        name: "username"
+    },
+    {
+        type: "list",
+        message: "What is your favorite color?",
+        choices: ["red", "blue", "pink", "purple", "green", "orange"],
+        name: "preferredColor"
+    }
+]).then(function (answers) {
+    console.log(answers)
+    axios.get(`https://api.github.com/users/${answers.username}?client_id=${client_id}&client_secret=${client_secret}`)
+        .then(function (response) {
+            console.log(response.data);
+            let name = response.data.name;
+            let profileImage = response.data.avatar_url;
+            let githubProfile = response.data.url;
+            let location = response.data.location;
+            let userBio = response.data.bio;
+            let publicRepo = response.data.public_repos;
+            let followers = response.data.followers;
+            let following = response.data.following;
+
+            console.log(name, profileImage, githubProfile, location, userBio, publicRepo, followers, following);
+            var html = createHTML({
+                title: 'example',
+                body: ` <h2 style="color:${answers.preferredColor};">${name}</h2> <img src= "${profileImage}"> <p>Repo Url: <a href="${githubProfile}">${answers.username}: </a></p> <p>Location: ${location}</> <p>User Bio: ${userBio}</p> <p>Public Repos: ${publicRepo}</p> <p>Follower: ${followers}</p> <p>Following: ${following}</p>`
+                
+                
+              })
+               
+              fs.writeFile('index.html', html, function (err) {
+                if (err) console.log(err)
+              })
+              
+        });
+});
+
+
